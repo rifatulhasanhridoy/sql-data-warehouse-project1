559682bd8f2ea793aa566e3f2f9fdc8f107678f4
Rifatul Hasan Hridoy <rifatulhasanhridoy@gmail.com>
2026-05-21 11:06:11 +0600

    Add files via upload

---
+++ Scripts/init_database.sql
@@ -0,0 +1,19 @@
+-- SELECT @@VERSION;
+-- CREATE DATABASE PracticeDB;
+-- USE PracticeDB;
+-- CREATE TABLE Demo (Id INT, Note VARCHAR(50));
+-- INSERT INTO Demo VALUES (1, 'Connection Successful!');
+-- SELECT * FROM Demo;
+
+
+CREATE DATABASE DataWarehouse;
+GO
+use DataWarehouse
+GO
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+
+
